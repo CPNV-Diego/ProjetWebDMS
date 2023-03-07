25691f60b94a73f9a310b9ea2e90cd6e5682c30c
CPNV-Diego <Diego.Pinto-Tomaz@cpnv.ch>
2023-03-07 16:53:09 +0100

    MAJ Journal de travail

--- Code/model/dataRegister.php
+++ Code/model/dataRegister.php
@@ -3,6 +3,13 @@ require "fileConnector.php";
 
 //region Global variables
 //endregion Global variables
+function saveRegister($data)
+{
+    //Cette fonction crée un tableau et l'écrit dans le json comme une ligne
+    $newRegisterToWrite = extractRegister($data);//crée tableau simple (non associatif)
+    writeRegisterInJSON($newRegisterToWrite);
+}
+
 function extractRegister($data)
 {
     $emailAddress = $data['inputEmailAddress'];
@@ -11,11 +18,4 @@ function extractRegister($data)
 
     $newRegisterTemp = array($emailAddress, $password);
     return $newRegisterTemp;
-}
-
-function saveRegister($data)
-{
-    //Cette fonction crée un tableau et l'écrit dans le json comme une ligne
-    $newRegisterToWrite = extractRegister($data);//crée tableau simple (non associatif)
-    writeRegisterInJSON($newRegisterToWrite);
 }
